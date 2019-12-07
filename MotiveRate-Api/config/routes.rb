f10c66e32656749cd4703a6cd8b234b7bc3e6bb5
Pascal Rassaby <pasbynumbers@gmail.com>
2019-12-07 04:49:07 +0000

    working on user controller

--- MotiveRate-Api/config/routes.rb
+++ MotiveRate-Api/config/routes.rb
@@ -1,3 +1,11 @@
+# Rails.application.routes.draw do
+#   scope '/api/v1' do
+#     resources :users
+#   end
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+    resources :users
+  
 end
