db4cb3a5fac34fe8c4ce4fd516803c1828f48fc0
Pascal Rassaby <pasbynumbers@gmail.com>
2019-12-21 15:24:48 +0000

    basic redux reducer and store added

--- motiverate-client/src/index.js
+++ motiverate-client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom'
+import './styles/index.css';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+console.log(store.getState())
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
