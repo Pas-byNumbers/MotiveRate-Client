68b7234079f980f557e1f77f642a346aed90bac1
Pascal Rassaby <pasbynumbers@gmail.com>
2020-01-08 23:28:29 +0000

    jwt encoding functional, seeds have encryption

--- MotiveRate-Api/app/controllers/api/v1/users_controller.rb
+++ MotiveRate-Api/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 require("byebug")
 
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create, :index, :show]
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET / users
@@ -17,7 +18,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user).serialized_json }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user).serialized_json, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
