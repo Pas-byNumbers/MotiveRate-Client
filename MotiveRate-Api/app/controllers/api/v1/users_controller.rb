7f8eca174ead533db3e07d8c21735f95e9a8c5a0
Pascal Rassaby <pasbynumbers@gmail.com>
2020-01-08 22:19:30 +0000

    can post to create new user

--- MotiveRate-Api/app/controllers/api/v1/users_controller.rb
+++ MotiveRate-Api/app/controllers/api/v1/users_controller.rb
@@ -1,69 +1,73 @@
-require('byebug')
+require("byebug")
 
-module Api::V1
-  class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
-    # GET / users
-    def index
+  # GET / users
+  def index
     @users = User.order(:id)
-    render json: 
-    UserSerializer.new(@users).serialized_json
-    end
+    render json: UserSerializer.new(@users).serialized_json
+  end
 
-    # GET / users / 1
-    def show
-    render json: 
-        UserSerializer.new(@user).serialized_json
-    end
+  # GET / users / 1
+  def show
+    render json: UserSerializer.new(@user).serialized_json
+  end
 
-    # POST / users
-    def create
-    @user = User.new(user_params)
-
-    if @user.save
-    render json: @user,
-    status: :created
-    else 
-        render json: @user.errors,
-        status: :unprocessable_entity
-    end
-    end
-
-    # PATCH / PUT / users / 1
-    def update
-    if @user.update(user_params)
-    render json: @user
-    else 
-        render json: @user.errors,
-        status: :unprocessable_entity
-    end
-    end
-
-    # DELETE / users / 1
-    def destroy
-    @user.destroy
-    if @user.destroy
-    head :no_content,
-    status: :ok
-    else 
-        render json: @user.errors,
-        status: :unprocessable_entity
-    end
-    end
-
-    # private
-    # # Use callbacks to share common setup or constraints between actions.
-    def set_user
-    @user = User.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params
-        .require(:user)
-        .permit(:username, :password_digest, :email, :first_name, :last_name, :score, :tier)
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user).serialized_json }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
-end
 
+  # POST / users
+  # def create
+  #   @user = User.new(user_params)
+
+  #   if @user.save
+  #     render json: @user,
+  #            status: :created
+  #   else
+  #     render json: @user.errors,
+  #            status: :unprocessable_entity
+  #   end
+  # end
+
+  # # PATCH / PUT / users / 1
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors,
+  #            status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE / users / 1
+  # def destroy
+  #   @user.destroy
+  #   if @user.destroy
+  #     head :no_content,
+  #          status: :ok
+  #   else
+  #     render json: @user.errors,
+  #            status: :unprocessable_entity
+  #   end
+  # end
+
+  # private
+  # # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params
+      .require(:user)
+      .permit(:username, :password, :email, :first_name, :last_name, :score, :tier)
+  end
+end
