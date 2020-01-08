9a4721683b52edb7f9d50a5d5f1050fb74bdb053
Pascal Rassaby <pasbynumbers@gmail.com>
2020-01-08 23:38:57 +0000

    hidden secret from app controller API

--- MotiveRate-Api/app/controllers/application_controller.rb
+++ MotiveRate-Api/app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::API
   before_action :authorized
 
-  def encode_token(payload)
-    # should store secret in env variable
-    JWT.encode(payload, 'my_s3cr3t')
+  def encode_token(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, "Rails.application.secrets.secret_key_base")
   end
 
   def auth_header
     # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
+    request.headers["Authorization"]
   end
 
   def decoded_token
     if auth_header
-      token = auth_header.split(' ')[1]
+      token = auth_header.split(" ")[1]
       # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+        JWT.decode(token, "Rails.application.secrets.secret_key_base", true, algorithm: "HS256")
       rescue JWT::DecodeError
         nil
       end
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::API
 
   def current_user
     if decoded_token
-      user_id = decoded_token[0]['user_id']
+      user_id = decoded_token[0]["user_id"]
       @user = User.find_by(id: user_id)
     end
   end
@@ -35,6 +35,6 @@ class ApplicationController < ActionController::API
   end
 
   def authorized
-    render json: { message: 'Please log in before access' }, status: :unauthorized unless logged_in?
+    render json: { message: "Please log in before access" }, status: :unauthorized unless logged_in?
   end
 end
