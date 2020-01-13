c547bc1e003b1f42d4721fd6cf3fa2c98722d212
Pascal Rassaby <pasbynumbers@gmail.com>
2020-01-13 21:54:34 +0000

    starting work on frontend auth

--- MotiveRate-Api/app/controllers/api/v1/users_controller.rb
+++ MotiveRate-Api/app/controllers/api/v1/users_controller.rb
@@ -19,28 +19,16 @@ class Api::V1::UsersController < ApplicationController
     render json: { user: UserSerializer.new(current_user).serialized_json }, status: :accepted
   end
 
+  # POST / users
   def create
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user).serialized_json, jwt: @token }, status: :created
     else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+      render json: { error: "failed to create user" }, status: :not_acceptable
     end
   end
-
-  # POST / users
-  # def create
-  #   @user = User.new(user_params)
-
-  #   if @user.save
-  #     render json: @user,
-  #            status: :created
-  #   else
-  #     render json: @user.errors,
-  #            status: :unprocessable_entity
-  #   end
-  # end
 
   # # PATCH / PUT / users / 1
   # def update
@@ -64,7 +52,7 @@ class Api::V1::UsersController < ApplicationController
   #   end
   # end
 
-  # private
+  private
   # # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
